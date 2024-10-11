3bc827fefa583b030fcb25e9cc5cd45b2da20f1d
abhivlogsingh <abhisheksingh4115@gmail.com>
2024-10-11 16:44:50 +0530

    updated time

--- src/loader/Time.jsx
+++ src/loader/Time.jsx
@@ -1,8 +1,8 @@
-// import moment from "moment";
+import moment from "moment";
 import React from "react";
 
 function Time({ time }) {
-  const videoTime = startOf("day")?.seconds(time)?.format("H:mm:ss");
+  const videoTime = moment()?.startOf("day")?.seconds(time)?.format("H:mm:ss");
   return (
     <div>
       <span className="absolute bottom-2 right-2 bg-black text-white px-2 py-1 text-xs rounded-md">
