dafa56dfeb56e71f1bca9c301ee9078d4e7d6a7a
abhivlogsingh <abhisheksingh4115@gmail.com>
2024-10-11 12:31:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { useAuth } from "./context/AuthProvider";
+
+function App() {
+  const {loading, data} = useAuth()
+  console.log(loading);
+  console.log(data);
+
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
